6e9e020755d7a4139878838c840a69f52c5775f0
Kabeer <98633317+Nameless3454@users.noreply.github.com>
2026-09-01 13:30:59 +0530

    Time: 19 ms (88.38%) | Memory: 53.4 MB (34.05%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        curr = head
+        while curr:
+            vals.append(curr.val)
+            curr = curr.next
+            
+        return vals == vals[::-1]
